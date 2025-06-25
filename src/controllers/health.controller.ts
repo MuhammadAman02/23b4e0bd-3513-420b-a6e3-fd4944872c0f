52e636215eb04a39278dc24c2faac044aa939c9d
MuhammadAman02 <mohammada@hst.ie>
2025-06-25 11:48:37 +0500

    update file

--- src/controllers/health.controller.ts
+++ src/controllers/health.controller.ts
@@ -6,11 +6,9 @@ export async function healthCheckHandler(
 ) {
   const healthData = {
     status: 'OK',
-    message: 'Server is running successfully',
     timestamp: new Date().toISOString(),
     uptime: process.uptime(),
   };
-
-  console.log('Health check requested:', healthData);
+  
   res.status(200).send(healthData);
 }
