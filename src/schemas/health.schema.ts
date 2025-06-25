5876fe8bb7ceb631249c48b29708772b436d2648
MuhammadAman02 <mohammada@hst.ie>
2025-06-25 11:48:34 +0500

    update file

--- src/schemas/health.schema.ts
+++ src/schemas/health.schema.ts
@@ -4,7 +4,6 @@ export const healthCheckSchema = {
   response: {
     200: z.object({
       status: z.string(),
-      message: z.string(),
       timestamp: z.string(),
       uptime: z.number(),
     }),
